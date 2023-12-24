88b30aff9224c9eea7ce4850a1a81fcf2b70dc6c
Anik-hasan07 <iamakashcse@gmail.com>
2023-12-25 03:16:53 +0600

    order section added

---
+++ backend/controllers/cartController.js
@@ -0,0 +1,48 @@
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const Cart = require("../models/cartModel");
+const ApiFeatures = require("../utils/apifeatures");
+const ErrorHandler = require("../utils/errorhander");
+
+exports.fetchCartByUser = catchAsyncErrors(async (req, res, next) => {
+    const { id } = req.user;
+    // req.body.user = req.user.id;
+    const cartItems = await Cart.find({ user: id }).populate('product');
+    res.status(201).json({
+        //   success: true,
+        cartItems,
+    });
+});
+exports.addToCart = catchAsyncErrors(async (req, res, next) => {
+    const { id } = req.user;
+    const cart = new Cart({ ...req.body, user: id });
+    const doc = await cart.save();
+    const result = await doc.populate('product');
+    res.status(201).json({
+        //   success: true,
+        result,
+    });
+});
+
+exports.deleteFromCart = async (req, res) => {
+    const { id } = req.params;
+    try {
+    const doc = await Cart.findByIdAndDelete(id);
+    res.status(200).json(doc);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
+
+exports.updateCart = async (req, res) => {
+    const { id } = req.params;
+    try {
+      const cart = await Cart.findByIdAndUpdate(id, req.body, {
+        new: true,
+      });
+      const result = await cart.populate('product');
+  
+      res.status(200).json(result);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  };
